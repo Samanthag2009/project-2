b9b6ee09ea87bf6674ace6b2e539f479ef213007
Samantha Gosselin <samanthag2009@gmail.com>
2023-03-21 21:11:06 -0500

    fixed typo, attempted to render home page in home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,17 @@
 const router = require('express').Router();
+const express = require('express');
 
+const app = express();
 
+// router.get('/', async (req, res) => {
+//     //render the homepage template as the response
+  
+//     res.render('homepage');
+//   });
+
+// GET Route for homepage
+app.get('/', (request, response) =>
+  response.sendFile(path.join(__dirname, '/public/homepage.handlebars'))
+);
 
 module.exports = router;
