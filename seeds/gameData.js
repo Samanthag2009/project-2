4824993bef43ae72b0553986b8f61902fbd4e33b
KrookedKickflip <colinnicosia84@yahoo.com>
2023-03-30 12:27:31 -0500

    added cover photo url to gameData

--- seeds/gameData.js
+++ seeds/gameData.js
@@ -6,7 +6,7 @@ const gameData = [
     {
         id: 1001,
         game_name: 'Halo',
-        image_url: 'https://thumbs2.imgbox.com/d0/aa/hnCgRGQt_t.png', 
+        image_url: 'https://thumbs2.imgbox.com/50/dc/1kU06KwZ_t.png', 
         genre: 'first-person shooter',
         game_description: 'Halo is a science fiction action game set in the twenty-sixth century, with the player assuming the role of the Master Chief, a cybernetically enhanced supersoldier.',
         rating: 5,
@@ -16,7 +16,7 @@ const gameData = [
     {
         id: 1002,
         game_name: 'Monster Hunter Rise',
-        image_url: '', 
+        image_url: 'https://thumbs2.imgbox.com/f6/60/H5fSXHob_t.png', 
         genre: 'RPG',
         game_description: 'Monster Hunter Rise is an action RPG that will transport players to the colorful Kamura Village, an all-new serene mountain locale that attracts visitors with its unique culture and innovative hunting technologies.',
         rating: 5,
@@ -26,7 +26,7 @@ const gameData = [
     {
         id: 1003,
         game_name: 'Silent Hill Downpour',
-        image_url: '', 
+        image_url: 'https://thumbs2.imgbox.com/8f/df/uL6tdVpG_t.png', 
         genre: 'Horror',
         game_description: 'Silent Hill: Downpour is a survival horror game played from the over-the-shoulder, third-person perspective, in which the player controls Murphy Pendleton, an escaped convict, as he navigates the titular supernatural town.',
         rating: 1,
@@ -36,7 +36,7 @@ const gameData = [
     {
         id: 1004,
         game_name: 'Minecraft',
-        image_url: '', 
+        image_url: 'https://thumbs2.imgbox.com/29/e1/e0wuCnh6_t.png', 
         genre: 'Open World',
         game_description: 'In Minecraft, players explore a blocky, procedurally generated, three-dimensional world with virtually infinite terrain and may discover and extract raw materials, craft tools and items, and build structures, earthworks, and machines.',
         rating: null,
@@ -46,7 +46,7 @@ const gameData = [
     {
         id: 1005,
         game_name: 'Barbie Horse Adventures',
-        image_url: '', 
+        image_url: 'https://thumbs2.imgbox.com/0e/24/Tt5zqjJb_t.png', 
         genre: 'RPG',
         game_description: "Barbie solves the mystery of the missing horse of Barbie's friend Teresa. To do so, the player, as Barbie, has to explore the countryside, consisting of varied terrain ranging from forest to desert, while solving logic puzzles.",
         rating: null,
@@ -56,7 +56,7 @@ const gameData = [
     {
         id: 1006,
         game_name: 'Ship of Fools',
-        image_url: '', 
+        image_url: 'https://thumbs2.imgbox.com/99/95/2lildRSh_t.png', 
         genre: 'Roguelite',
         game_description: 'Set sail, man the cannons and battle sea monsters through the tempestuous seas of the Archipelago. Are you fool enough to stop the Aquapocalypse?',
         rating: 4,
@@ -66,7 +66,7 @@ const gameData = [
     {
         id: 1007,
         game_name: 'Kingdom Hearts',
-        image_url: '', 
+        image_url: 'https://thumbs2.imgbox.com/8f/a6/R9hYV2gE_t.png', 
         genre: 'RPG',
         game_description: 'Set in a vast array of Disney and Pixar worlds, KINGDOM HEARTS follows the journey of Sora, a young boy and unknowing heir to a spectacular power.',
         rating: 5,
@@ -76,7 +76,7 @@ const gameData = [
     {
         id: 1008,
         game_name: 'Splatoon',
-        image_url: '', 
+        image_url: 'https://thumbs2.imgbox.com/c2/04/omoRXrMx_t.png', 
         genre: 'Third-person shooter',
         game_description: 'Ink up the. Splatlands. Enter a sun-scorched desert inhabited by battle-hardened Inklings and Octolings. Ink, dive, swim, and splat your way to the top!',
         rating: null,
@@ -86,7 +86,7 @@ const gameData = [
     {
         id: 1009,
         game_name: 'Umineko When They Cry',
-        image_url: '', 
+        image_url: 'https://thumbs2.imgbox.com/92/8f/GZgu5xTB_t.png', 
         genre: 'Visual Novel',
         game_description: '"When the Seagulls Cry" is a Japanese dōjin soft visual novel series produced by 07th Expansion. Its first episode debuted at Comiket 72 for Windows in August 2007. The story focuses on a group of eighteen people on a secluded island for a period of two days, and the mysterious murders that befall them.',
         rating: 4,
@@ -96,7 +96,7 @@ const gameData = [
     {
         id: 1010,
         game_name: 'Pokemon Y',
-        image_url: '', 
+        image_url: 'https://thumbs2.imgbox.com/d4/29/HXzotR1Y_t.png', 
         genre: 'RPG',
         game_description: 'This time, the games take place in the Kalos region—based on France—with the objective being to thwart the schemes of the nefarious criminal organization Team Flare, all while attempting to challenge the Pokémon League Champion.',
         rating: 5,
@@ -106,7 +106,7 @@ const gameData = [
     {
         id: 1011,
         game_name: "Dante's Inferno",
-        image_url: '', 
+        image_url: 'https://thumbs2.imgbox.com/bb/03/7MrsJ82W_t.png',
         genre: 'RPG',
         game_description: 'A Journey of the 9 circles of Hell.',
         rating: 3,
